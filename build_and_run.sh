3b2ba81f317425e9e6ffa77d40d12381df112677
xbexbex <xbexbex@users.noreply.github.com>
2025-03-05 15:25:32 +0200

    Remove beam_size param

--- build_and_run.sh
+++ build_and_run.sh
@@ -7,7 +7,6 @@ docker run --name=faster-whisper --rm \
   -e TZ=Europe/Helsinki \
   -e WHISPER_MODEL=large-v3 \
   -e WHISPER_COMPUTE_TYPE=int8 \
-  -e WHISPER_BEAM_SIZE=5 \
   -p 10300:10300 \
   -v /home/xbexbex/git/transcribe-docker/recordings:/recordings \
   -v /home/xbexbex/git/transcribe-docker/transcriptions:/transcriptions \
